8416be1dbb0d11ec73e427a7b1566be1237cadef
TS0713 <tsp.0713@gmail.com>
2024-05-18 15:01:21 +0530

    requirements added

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import setuptools
 
-
 with open("README.md","r",encoding="utf-8") as f:
     long_description = f.read()
 
@@ -9,22 +8,24 @@ __version__ = "0.0.0"
 REPO_NAME = "mlflow-end2end"
 AUTHOR_USER_NAME = "TS0713"
 SRC_REPO = "mlproject"
-AUTHOR_EMAIL = "tsp0713@gmail.com"
+AUTHOR_EMAIL = "tsp.0713@gmail.com"
 
-# It will look for src/mlproject and look for __init__.py inside each subfolder
-# and convert those subfolders into packages
+
 
 setuptools.setup(
     name = SRC_REPO,
     version = __version__,
     author = AUTHOR_USER_NAME,
     author_email = AUTHOR_EMAIL,
-    description = "A small python package setup for mlflow",
+    description = "A small python package for ML App",
     long_description = long_description,
     url = f"htpps://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    package_dir = {"":"src"},
-    packages = setuptools.find_packages(where="src")
+    project_urls = {
+        "Bug Tracker": f"htpps://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
+    },
+    package_dir={"":"src"},
+    packages = setuptools.find_packages(where = "src")
+
 )
 
 
-#print (long_description)
